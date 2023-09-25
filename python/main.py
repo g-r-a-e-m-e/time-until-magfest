6ed18e59c63a068f5c0fe5ca5463bfe55623312e
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2023-09-24 21:15:58 -0400

    Create main.py

---
+++ python/main.py
@@ -0,0 +1,46 @@
+###
+### This bot prints the number of days, hours, and minutes until MAGFest 2024!
+###
+### made by @g-r-a-e-m-e
+
+# Boilerplate
+import arrow
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+### Helper functions ###
+# Get the current time based on local time
+def get_current_time():
+    # Current time in UTC
+    utc = arrow.utcnow()
+    # Return local time
+    return utc.to('local')
+
+def get_message(current_datetime, future_datetime):
+    # Specify message
+    message = future_datetime.humanize(current_datetime, 
+                                   only_distance = True,
+                                   granularity = ['day', 'hour', 'minute'])
+    return message
+
+# Load .env file
+load_dotenv()
+
+# Get the token
+token = os.getenv('DISCORD_TOKEN')
+
+# Create the client
+bot = commands.Bot(command_prefix = '!', intents = discord.Intents.all())
+
+### Commands ###
+@bot.hybrid_command(name = 'time_until_magfest')
+async def time_until(command):
+    # Get current time
+    now = get_current_time()
+    message = f"There are {get_message(now, arrow.get('2024-01-24'))} until MAGFest 2024!"
+    await command.send(message)
+
+# Run!
+bot.run(token)
